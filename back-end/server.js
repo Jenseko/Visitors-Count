62a19d9c9bd6332e908ed3ae36e7102c4fa3e4eb
Jenseko <jenscode23@gmail.com>
2023-06-13 10:45:31 +0200

    frontend

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,31 @@
 import express from 'express';
+import fs from 'fs-jetpack';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-let counter = 0;
+// readFile
+fs.file('./count.txt');
+
+// Erstellen einer base URL, 
+let { pathname } = new URL('./count.txt', import.meta.url);
+// stelle sicher, dass die Datei existiert, wenn nicht dann erstelle diese
+let prevCount = fs.read(pathname); // read() gibt immer einen String zurück
+
+console.log(pathname);
+console.log({ prevCount: Number(prevCount) });
+
+// Wandle String in eine Zahl um, wenn kein String bereits besteht, nehme Wert 0
+let counter = Number(prevCount) || 0;
+
+// Edge Cases - sichergehen, dass wenn Programm von jemand anders gestartet wird, 
+// muss man sichergehen, dass ein individueller Link mit einem absoluten Pfad erstellt wird
+
 
 app.get('/visit', (req, res) => {
     counter++;
     res.send();
+    fs.write(pathname, `${counter}`);
 })
 
 app.get('/visited', (req, res) => {
